5b445ca6490e03d8ec91ada33d9230f613b53a06
qTAB <channelbarrel@gmail.com>
2022-01-30 22:13:22 +0300

    Initial commit

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -9,6 +9,15 @@ window::window(const char* title, const int width, const int height)
 
 void window::loop()
 {
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
 	while (!glfwWindowShouldClose(mainWindow)) {
 		glClearColor(0.0f, 0.7f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
